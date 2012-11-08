516c53fa5a33fe16ff1334317f9af0856899c16f
Tatsuro Baba <harakirisoul@gmail.com>
2012-11-08 23:28:58 +0900

    user can edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @articles = Article.order('published_at desc').page page
   end
 
+  def show id
+    @article = Article.find id
+  end
+
   def new
     @article = Article.new
   end
@@ -15,6 +19,20 @@ class ArticlesController < ApplicationController
       redirect_to articles_path, notice: 'New article was successfully created!'
     else
       render 'new'
+    end
+  end
+
+  def edit id
+    @article = Article.find id
+  end
+
+  def update id, article
+    @article = Article.find id
+
+    if @article.update_attributes article
+      redirect_to @article, notice: 'Article was successfully updated!'
+    else
+      render 'edit'
     end
   end
 end
